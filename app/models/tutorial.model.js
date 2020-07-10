d7aac979129c6f55156581ac7e20b75bcb2c695d
sriramaravind <65369583+sriramaravind@users.noreply.github.com>
2020-07-10 17:00:58 +0530

    NSA Server1 Commit

---
+++ app/models/tutorial.model.js
@@ -0,0 +1,19 @@
+module.exports = mongoose => {
+  var schema = mongoose.Schema(
+    {
+      name: String,
+      date: String,
+      time: String,
+    },
+    { timestamps: true }
+  );
+
+  schema.method("toJSON", function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Tutorial = mongoose.model("tutorial", schema);
+  return Tutorial;
+};
